987d06a9e70808e233839bd48cfbb1364d34a2ac
psabater <psabater@okode.com>
2017-11-14 10:00:29 +0100

    Import in the main index the component resources

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export * from './mmobile';
 export * from './log';
 export * from './config';
 export * from './crashlytics';
+export * from './components';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './mmobile';
 export * from './log';
 export * from './config';
 export * from './crashlytics';
+export * from './components'
