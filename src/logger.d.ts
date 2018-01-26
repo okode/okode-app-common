5f8e3399ca1e54b6568a875798e26e4215ddbc5b
Pedro Jorquera <pjorquera@okode.com>
2018-01-26 10:03:12 +0100

    Refactor

--- src/logger.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Logger {
-    i(message?: any, ...optionalParams: any[]): any;
-    d(message?: any, ...optionalParams: any[]): any;
-    w(message?: any, ...optionalParams: any[]): any;
-    e(message?: any, ...optionalParams: any[]): any;
-}
