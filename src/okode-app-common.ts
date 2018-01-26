bad79297f0dc152433f0d21818ce2a8a9a14696a
Pedro Jorquera <pjorquera@okode.com>
2018-01-26 10:08:47 +0100

    Added module definition

---
+++ src/okode-app-common.ts
@@ -0,0 +1,27 @@
+import { Observable } from 'rxjs';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { MyComponent } from './components/my-component';
+import { Config } from './providers/config';
+import { CrashlyticsErrorHandler } from './providers/crashlytics';
+import { Log } from './providers/log';
+import { MMobile } from './providers/mmobile';
+
+@NgModule({
+  declarations: [
+    // declare all components that your module uses
+    MyComponent
+  ],
+  exports: [
+    // export the component(s) that you want others to be able to use
+    MyComponent
+  ]
+})
+export class OkodeAppCommonModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: OkodeAppCommonModule,
+      providers: [ Config, CrashlyticsErrorHandler, Log, MMobile ]
+    };
+  }
+}
+
