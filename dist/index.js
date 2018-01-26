95e3b7991ac5ba34f325e99b17b60f99d0f59c51
Pedro Jorquera <pjorquera@okode.com>
2018-01-26 11:05:27 +0100

    Added missing exportds

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,5 @@ export * from './providers/mmobile';
 export * from './providers/log';
 export * from './providers/config';
 export * from './providers/crashlytics';
+export * from './okode-app-common';
 //# sourceMappingURL=index.js.map
