4f27a2481a8f3caa004a05bc2408adbd38fd906a
alexdiaz <adiaz@okode.com>
2018-02-19 18:31:01 +0100

    Fixed index exports

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,6 @@ export * from './providers/mmobile';
 export * from './providers/log';
 export * from './providers/config';
 export * from './providers/crashlytics';
+export * from './providers/http-cache-interceptor';
 export * from './okode-common';
 //# sourceMappingURL=index.js.map
