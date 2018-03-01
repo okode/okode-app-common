c8d7cd1dcf4fbd8df6aeedfc5ae4dbc4dda164c5
Harut Yeritsyan <hyeritsyan@okode.com>
2018-03-01 18:14:27 +0100

    Added HTTP Interceptor Cache storing to Ionic Native Storage

--- dist/providers/http-cache-interceptor.d.ts
+++ dist/providers/http-cache-interceptor.d.ts
@@ -1,14 +1,21 @@
 import { InjectionToken } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { Storage } from '@ionic/storage';
 export declare const HTTP_CACHE_INTERCEPTOR_DURATION_MINS: InjectionToken<number>;
+export declare const HTTP_CACHE_INTERCEPTOR_STORE_CACHE: InjectionToken<boolean>;
 export declare class HttpCacheInterceptor implements HttpInterceptor {
+    private storage;
     private cache;
     private durationMins;
+    private storeCache;
     private static readonly HEADER_NAME;
     private static readonly HEADER_VALUE_CACHE_RESPONSE;
     private static readonly HEADER_VALUE_CACHE_CLEAR;
-    constructor(duration: number);
+    constructor(duration: number, store: boolean, storage: Storage);
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
     private isReponseExpired(cachedResponse);
+    private loadCacheFromStorage();
+    private setToCache(key, value);
+    private clearCache();
 }
--- src/providers/http-cache-interceptor.ts
+++ src/providers/http-cache-interceptor.ts
@@ -1,8 +1,11 @@
-import { Injectable, Inject, InjectionToken } from '@angular/core';
+import { Injectable, Inject, InjectionToken, isDevMode } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { Storage } from '@ionic/storage';
 
 export const HTTP_CACHE_INTERCEPTOR_DURATION_MINS = new InjectionToken<number>('httpCache.mins');
+export const HTTP_CACHE_INTERCEPTOR_STORE_CACHE = new InjectionToken<boolean>('httpCache.store');
+const HTTP_CACHE_STORAGE = 'HTTP_CACHE_STORAGE';
 
 @Injectable()
 export class HttpCacheInterceptor implements HttpInterceptor {
@@ -10,12 +13,21 @@ export class HttpCacheInterceptor implements HttpInterceptor {
   private cache = new Map<string, { response: HttpResponse<any>, timestamp: number }>();
 
   private durationMins = null;
+  private storeCache = null;
   private static readonly HEADER_NAME = 'Cache-Interceptor';
   private static readonly HEADER_VALUE_CACHE_RESPONSE = 'cache-response';
   private static readonly HEADER_VALUE_CACHE_CLEAR = 'clear-cache';
 
-  constructor(@Inject(HTTP_CACHE_INTERCEPTOR_DURATION_MINS) duration: number) {
+  constructor(
+    @Inject(HTTP_CACHE_INTERCEPTOR_DURATION_MINS) duration: number,
+    @Inject(HTTP_CACHE_INTERCEPTOR_STORE_CACHE) store: boolean,
+    private storage: Storage
+  ) {
     this.durationMins = duration || null;
+    if (isDevMode() && store) {
+      this.storeCache = true;
+      this.loadCacheFromStorage();
+    }
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -27,7 +39,7 @@ export class HttpCacheInterceptor implements HttpInterceptor {
     }
 
     if (headerValue == HttpCacheInterceptor.HEADER_VALUE_CACHE_CLEAR) {
-      this.cache.clear();
+      this.clearCache();
       return next.handle(req);
     }
 
@@ -40,7 +52,7 @@ export class HttpCacheInterceptor implements HttpInterceptor {
 
     return next.handle(req).do(event => {
       if (event instanceof HttpResponse && cacheResponse) {
-        this.cache.set(req.urlWithParams, { response: event.clone(), timestamp: Date.now() });
+        this.setToCache(req.urlWithParams, { response: event.clone(), timestamp: Date.now() });
       }
     });
   }
@@ -50,4 +62,27 @@ export class HttpCacheInterceptor implements HttpInterceptor {
     return Date.now() > cachedResponse.timestamp + (this.durationMins * 60000);
   }
 
+  private async loadCacheFromStorage() {
+    await this.storage.ready();
+    let cacheFromStorage = await this.storage.get(HTTP_CACHE_STORAGE);
+    if (cacheFromStorage) {
+      this.cache = <Map<string, { response: HttpResponse<any>, timestamp: number }>>JSON.parse(cacheFromStorage);
+    }
+  }
+
+  private setToCache(key: string, value: { response: HttpResponse<any>, timestamp: number }) {
+    this.cache.set(key, value);
+    if (this.storeCache) {
+      this.storage.ready()
+        .then(() => this.storage.set(HTTP_CACHE_STORAGE, JSON.stringify(this.cache)));
+    }
+  }
+
+  private async clearCache() {
+    this.cache.clear();
+    if (this.storeCache) {
+      this.storage.ready()
+        .then(() => this.storage.remove(HTTP_CACHE_STORAGE));
+    }
+  }
 }
