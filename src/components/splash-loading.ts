de08e5aef9fbe0ee397ad467322e2da5c8ccffee
psabater <psabater@okode.com>
2018-05-15 13:12:27 +0200

    Adding splash scss and html

--- src/components/splash-loading.ts
+++ src/components/splash-loading.ts
@@ -4,47 +4,8 @@ import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'splash-loading-component',
-  template: `
-  <ion-content padding>
-  <div class="loading">
-    <ion-spinner></ion-spinner><span>Loading...</span>
-  </div>
-  </ion-content>
-  `,
-  styles:[`
-  $splash-loading-background-color: color($colors, primary);
-  $splash-loading-background-image: none;
-  ion-content {
-    .scroll-content {
-      background-color: $splash-loading-background-color;
-      background-image: $splash-loading-background-image;
-      background-size: cover;
-      text-align: center;
-      color: white;
-      padding-bottom: 100px !important;
-      overflow-y: hidden !important;
-      min-height: 500px;
-    }
-    .loading {
-      width: 100%;
-      height: 100%;
-      margin: 50% auto;
-    }
-    ion-spinner {
-      display: block;
-      margin: 10px auto;
-      * {
-        stroke: white !important;
-      }
-  
-      &.spinner-circles circle,
-      &.spinner-bubbles circle,
-      &.spinner-dots circle {
-        fill: white !important;
-      }
-    }
-  }
-  `]
+  templateUrl: './splash-loading.html',
+  styleUrls: ['./splash-loading.scss']
 })
 export class SplashLoadingComponent {
 
