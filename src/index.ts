031e95205aca4cac33602e0faf0a5814a38b8d08
psabater <psabater@okode.com>
2018-05-15 12:57:13 +0200

    Adding missing splash-loading import

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './providers/config';
 export * from './providers/crashlytics';
 export * from './providers/http-cache-interceptor';
 export * from './components/modal-wrapper';
+export * from './components/splash-loading';
 export * from './okode-common';
